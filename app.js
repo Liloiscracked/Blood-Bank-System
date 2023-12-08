af3e0c3a2ab1ec05a59ec803961d46d38c1900e2
Liloiscracked <mr.moodyaljoudy@gmail.com>
2023-12-08 16:05:41 +0300

    added files to work on(Add the front yourself !!)

--- app.js
+++ app.js
@@ -20,29 +20,78 @@ var db = mongoose.connection;
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
 
+
+//the donor applicaation details
 app.post("/donor",(req,res)=>{
     var name = req.body.name;
     var email = req.body.email;
     var phno = req.body.phno;
     var password = req.body.password;
+    var Blood_type = req.body.Blood_type;
+    var Ssn = req.body.Ssn;
 
     var data = {
         "ID": name,
         "email" : email,
         "phno": phno,
-        "password" : password
+        "password" : password,
+        "Blood_type":Blood_type,
+        "Ssn": Ssn
     }
-
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
-            throw err;
+    async function Handle(data){  
+        try {
+          const query = data;
+          const document = await db.collection('Donor').findOne(query);
+          if (document){
+            console.log('Document exists:', document);
+            return res.redirect('donor.html');
+          } else {
+            db.collection('Donor').insertOne(data);
+            return res.redirect('Done.html');
+          }
+        } finally {
+          console.log('Your application has been recieved');
         }
-        console.log("Record Inserted Successfully");
-    });
-
-    return res.redirect('signup_success.html');
-
+      }
+      Handle(data);
 })
+
+//the donor applicaation details
+app.post("/recipient",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+    var Blood_type = req.body.Blood_type;
+    var Ssn = req.body.Ssn;
+
+    var data = {
+        "ID": name,
+        "email" : email,
+        "phno": phno,
+        "password" : password,
+        "Blood_type":Blood_type,
+        "Ssn": Ssn
+    }
+    async function Handle(data){  
+        try {
+          const query = data;
+          const document = await db.collection('recipient').findOne(query);
+          if (document){
+            console.log('Document exists:', document);
+            return res.redirect('donor.html');
+          } else {
+            db.collection('recipient').insertOne(data);
+            return res.redirect('Done.html');
+          }
+        } finally {
+          console.log('Your application has been recieved');
+        }
+      }
+      Handle(data);
+})
+
+
 app.get('/donor',(req,res)=>{
   res.sendFile('public/donor.html');
 })
@@ -51,7 +100,7 @@ app.get("/",(req,res)=>{
     res.set({
         "Allow-access-Allow-Origin": '*'
     })
-    return res.redirect('donor.html');
+    return res.redirect('Home.html');
 }).listen(3000);
 
 
